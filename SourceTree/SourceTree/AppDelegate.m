2875b41a5c9e9db89363313421fc07a9b09e1efa
lanou3g <lanou3g@lanou3gdeMac-mini.local>
2015-11-09 10:38:38 +0800

    11.9 李安宁提交

--- SourceTree/SourceTree/AppDelegate.m
+++ SourceTree/SourceTree/AppDelegate.m
@@ -16,7 +16,9 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    int a = 0;
+    
     return YES;
 }
 
